993d8b8c33c9bef6d66a918f9ab51722306392db
Joseph Blasingame <blasingame.joseph@gmail.com>
2022-02-13 03:12:36 -0600

    restore caching for members page (lesson 167)

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -19,10 +19,15 @@ const httpOptions = {
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
   constructor(private http: HttpClient) { }
 
   getMembers(userParams: UserParams) {
+    var response = this.memberCache.get(Object.values(userParams).join("-"));
+    if (response) {
+      return of(response);
+    }
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append("minAge", userParams.minAge.toString());
@@ -31,7 +36,11 @@ export class MembersService {
     params = params.append("orderBy", userParams.orderBy);
 
     // if (this.members.length > 0) return of(this.members);
-    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params)
+      .pipe(map(response => {
+        this.memberCache.set(Object.values(userParams).join("-"), response);
+        return response;
+      }))
   }
 
 
